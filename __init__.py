e26479f47588ec1996993bb3cf1b63ac541f731a
jodyngo <jodyngo@gmail.com>
2024-12-05 13:56:40 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,16 +1,6 @@
 from .nuscenes_dataset import NuScenesSegDataset
 from .loading import SegLabelMapping
-from .minkunet_segmentor import MinkUNetSegmentor
-from .data_preprocessor_singledistance import MinkNuscDataPreprocessorSingle
-from .mink_nusc_head_singledistance import MinkNuscHeadSD
-from .distancedCELoss import DistancedCrossEntropyLoss
-from .mldas_dataset import MLDASSegDataset
 
 __all__ = ['NuScenesSegDataset',
            'SegLabelMapping',
-           'MinkUNetSegmentor',
-           'MinkNuscDataPreprocessorSingle',
-           'MinkNuscHeadSD',
-           'DistancedCrossEntropyLoss',
-           'MLDASSegDataset'
 ]
